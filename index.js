4fb0f46a41655cb0294dbd7462991a39afff4d86
sungho Moon <moon@7959.ml>
2020-10-04 16:23:14 +0900

    gw

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ Array.from(document.querySelectorAll('.pill')).forEach(el => {
             case 'JavaScript':
                 document.querySelector('.about-skilled .desc').innerHTML = '목표 숙련도<div class="progress" data-percent="85.7"></div><br>자바스크립트는 웹 브라우저 내에서 주로 사용되는 스크립트 프로그래밍 언어입니다. <a href="https://ko.wikipedia.org/wiki/자바스크립트" target="_blank">위키백과</a>'
                 break
-
             case 'HTML':
-                document.querySelector('.about-skilled .desc').innerHTML = '목표 숙련도<div class="progress" data-percent="99.9"></div><br>HTML 초보입니다. <a href="https://ko.wikipedia.org/wiki/HTML" target="_blank">위키백과</a>'
+                document.querySelector('.about-skilled .desc').innerHTML = '목표 숙련도<div class="progress" data-percent="99.8"></div><br>HTML 초보입니다. <a href="https://ko.wikipedia.org/wiki/HTML" target="_blank">위키백과</a>'
                 break
             case 'CSS':
                 document.querySelector('.about-skilled .desc').innerHTML = '목표 숙련도<div class="progress" data-percent="84.8"></div><br>CSS는 마크업 언어가 실제 표시되는 방법을 기술하는 언어입니다. <a href="https://ko.wikipedia.org/wiki/종속형_시트" target="_blank">위키백과</a>'
